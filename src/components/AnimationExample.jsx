b0dc8685e6c694fe49eb8a3e89c1e942f63905f6
LeoAsher25 <leoasher2507@gmail.com>
2023-05-17 14:39:53 +0700

    add comments

--- src/components/AnimationExample.jsx
+++ src/components/AnimationExample.jsx
@@ -4,10 +4,15 @@ import { useSpring, animated } from "react-spring";
 const AnimationExample = () => {
   const [isAnimating, setIsAnimating] = useState(false);
 
+  // hook `useSpring` để tạo ra một SpringValues để truyền vào `animated` component.
+  // Khi component mounted, các thuộc tính trong SpringValues sẽ được áp dụng
   const boxAnimation = useSpring({
     opacity: isAnimating ? 1 : 0,
     transform: isAnimating ? "translateY(0)" : "translateY(-100%)",
   });
+
+  // Ở đây chúng ta dùng state `isAnimating` để diều khiên trạng thái của animation
+  // và 1 button để toggle giá trị của `isAnimating`
 
   return (
     <div>
@@ -16,7 +21,7 @@ const AnimationExample = () => {
       </button>
       <animated.div
         style={{
-          ...boxAnimation,
+          ...boxAnimation, // truyền SpringValues cho `animate` component
           width: "200px",
           height: "200px",
           backgroundColor: "red",
